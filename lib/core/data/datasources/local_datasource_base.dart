63fc1e15c11bb0d8b8caa99da9e050e7fd79fb28
Raymond Yegon <raymondyegon17@gmail.com>
2024-04-28 16:55:01 +0300

    feat: Generic function for local and remote datasources

---
+++ lib/core/data/datasources/local_datasource_base.dart
@@ -0,0 +1,8 @@
+// Dart imports:
+import 'dart:async';
+
+abstract class LocalDatasource<T> {
+  // ignore: close_sinks
+  final StreamController<T> dataStreamController = StreamController<T>();
+  void dispose();
+}
---
+++ lib/core/data/datasources/remote_datasource_base.dart
@@ -0,0 +1,8 @@
+// Dart imports:
+import 'dart:async';
+
+abstract class RemoteDatasource<T> {
+  // ignore: close_sinks
+  final StreamController<T> dataStreamController = StreamController<T>();
+  void dispose();
+}
